dbbd464f669ba4999a8e92f329c85eea91c1fd8c
heydaytran <thinhyl99@gmail.com>
2020-12-07 13:32:32 +0700

    add project

---
+++ src/java/org/yournamehere/client/MainEntryPoint.java
@@ -0,0 +1,27 @@
+
+package org.yournamehere.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+
+public class MainEntryPoint implements EntryPoint {
+    public MainEntryPoint() {
+    }
+    public void onModuleLoad() {
+        final Label label = new Label("Hello, GWT!!!");
+        final Button button = new Button("Click me!");
+        
+        button.addClickHandler(new ClickHandler() {
+            public void onClick(ClickEvent event) {
+                label.setVisible(!label.isVisible());
+            }
+        });
+        
+        RootPanel.get().add(button);
+        RootPanel.get().add(label);
+    }
+}
